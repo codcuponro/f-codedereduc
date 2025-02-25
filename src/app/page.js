5c6c6a56349ea501cbd05296f8a81972bb74a1a1
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-25 22:56:57 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -5,28 +5,28 @@ import FavoritesCoupons from "@/templates/home-page/favorites-coupons";
 import FavoriteStores from "@/templates/home-page/favorites-stores";
 import HowItWorks from "@/templates/home-page/how-it-works";
 import TopCategories from "@/templates/home-page/top-categories";
-import { getCategories, getCouponAndDeals, getExclusiveCoupon, getFavoritesCoupon, getStores } from "../services";
+// import { getCategories, getCouponAndDeals, getExclusiveCoupon, getFavoritesCoupon, getStores } from "../services";
 
 
 
 export default async function Home() {
   
   const pageData = await getHomPage()
-  const stores = await getStores()
-  const categories = await getCategories()
-  const couponAndDeals = await getCouponAndDeals()
-  const favoritesCoupon = await getFavoritesCoupon()
-  const exclusiveCoupon = await getExclusiveCoupon()
+  // const stores = await getStores()
+  // const categories = await getCategories()
+  // const couponAndDeals = await getCouponAndDeals()
+  // const favoritesCoupon = await getFavoritesCoupon()
+  // const exclusiveCoupon = await getExclusiveCoupon()
 
 
   return (
     <>
       <LandingPageSlider data={exclusiveCoupon}/>
-      <FavoritesCoupons data={favoritesCoupon} />
+      {/* <FavoritesCoupons data={favoritesCoupon} />
       <FavoriteStores data={stores}/>
       <CouponsAndDeals data={couponAndDeals}/>
       <TopCategories data={categories}/>
-      <HowItWorks data={pageData}/>
+      <HowItWorks data={pageData}/> */}
     </>
   );
 }
