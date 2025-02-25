d01c366dbc5b25cf2c78525dbc09c87c7944ed70
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-25 23:21:38 +0500

    3

--- src/config/Axios.js
+++ src/config/Axios.js
@@ -1,17 +1,20 @@
 export const Request = async (endpoint, options = {}) => {
   const url = `${process.env.NEXT_PUBLIC_BACKEND_PUBLIC_BASE_URL}/api${endpoint}`;
-  const defaultOptions = {
-    method: 'GET',
-    cache: "no-store",
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    signal: AbortSignal.timeout(60 * 1000),
-    ...options
-  };
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 30000);
 
   try {
-    const response = await fetch(url, defaultOptions);    
+    const response = await fetch(url, {
+      method: 'GET',
+      cache: "no-store",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      signal: controller.signal,
+      ...options
+    });
+
+    clearTimeout(timeout);
     return await response.json();
   } catch (error) {
     console.error('Fetch error:', error);
