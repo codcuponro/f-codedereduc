648ea3fa6d6aa359ed50f1dec0ad75a07b3cfc25
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-25 22:58:55 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import LandingPageSlider from "@/components/main/landing-page-slider";
-import { getHomPage } from "@/services";
-import CouponsAndDeals from "@/templates/home-page/coupons-and-deals";
-import FavoritesCoupons from "@/templates/home-page/favorites-coupons";
-import FavoriteStores from "@/templates/home-page/favorites-stores";
-import HowItWorks from "@/templates/home-page/how-it-works";
-import TopCategories from "@/templates/home-page/top-categories";
+// import LandingPageSlider from "@/components/main/landing-page-slider";
+// import { getHomPage } from "@/services";
+// import CouponsAndDeals from "@/templates/home-page/coupons-and-deals";
+// import FavoritesCoupons from "@/templates/home-page/favorites-coupons";
+// import FavoriteStores from "@/templates/home-page/favorites-stores";
+// import HowItWorks from "@/templates/home-page/how-it-works";
+// import TopCategories from "@/templates/home-page/top-categories";
 // import { getCategories, getCouponAndDeals, getExclusiveCoupon, getFavoritesCoupon, getStores } from "../services";
 
 
@@ -21,12 +21,13 @@ export default async function Home() {
 
   return (
     <>
-      <LandingPageSlider data={exclusiveCoupon}/>
+      {/* <LandingPageSlider data={exclusiveCoupon}/> */}
       {/* <FavoritesCoupons data={favoritesCoupon} />
       <FavoriteStores data={stores}/>
       <CouponsAndDeals data={couponAndDeals}/>
       <TopCategories data={categories}/>
       <HowItWorks data={pageData}/> */}
+      Home Page
     </>
   );
 }
