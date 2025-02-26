90c43214b4438b4161896894e3c2f12ecff797ce
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-26 15:32:02 +0500

    update

--- src/config/Axios.js
+++ src/config/Axios.js
@@ -1,7 +1,7 @@
 export const Request = async (endpoint, options = {}) => {
   const url = `${process.env.NEXT_PUBLIC_BACKEND_PUBLIC_BASE_URL}/api${endpoint}`;
   const controller = new AbortController();
-  const timeout = setTimeout(() => controller.abort(), 30000);
+  // const timeout = setTimeout(() => controller.abort(), 30000);
 
   try {
     const response = await fetch(url, {
@@ -14,7 +14,7 @@ export const Request = async (endpoint, options = {}) => {
       ...options
     });
 
-    clearTimeout(timeout);
+    // clearTimeout(timeout);
     return await response.json();
   } catch (error) {
     console.error('Fetch error:', error);
