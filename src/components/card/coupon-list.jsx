d4019ed47e7e9afbbbb517f9b266097f407d1cb1
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-27 18:54:27 +0500

    updation

--- src/components/card/coupon-list.jsx
+++ src/components/card/coupon-list.jsx
@@ -10,8 +10,8 @@ const CouponList = ({ logo, item }) => {
         <div className={`border border-[#DEE2E6] overflow-hidden rounded-lg`}>
             <div className='flex flex-row gap-3 sm:gap-4 sm:justify-center items-center md:gap-[27px]'>
                 {
-                    logo ? <figure className='md:border-r flex justify-center items-start sm:items-center'>
-                        <Image src={item?.Icon?.url || item?.store?.Icon?.url || "/images/fallback.png"} alt='' width={125} height={125} className='scale-y-105 w-[170px] md:w-[125px]' />
+                    logo ? <figure className='md:border-r flex justify-center items-start p-4 sm:p-0 sm:items-center'>
+                        <Image src={item?.Icon?.url || item?.store?.Icon?.url || "/images/fallback.png"} alt='' width={125} height={125} className='scale-y-105 w-[110px] rounded-lg sm:rounded-none sm:w-[170px] md:w-[125px]' />
                     </figure>
                         : <h6 className={`font-extrabold text-[34px] flex flex-col pl-[30px] py-[26px] leading-8 ${disabled ? 'text-[#505050]' : 'text-primary'}`} >
                             {item?.DiscountValue}
