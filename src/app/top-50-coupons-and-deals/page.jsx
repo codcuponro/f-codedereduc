fa0fc13dc40678f16b249b0690cb623862142358
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-27 22:07:09 +0500

    client side

--- src/app/top-50-coupons-and-deals/page.jsx
+++ src/app/top-50-coupons-and-deals/page.jsx
@@ -1,16 +1,29 @@
+"use client"
 import Breadcrumb from '@/components/breadcrumb'
 import CategoryButton from '@/components/card/category-button'
 import CouponList from '@/components/card/coupon-list'
 import Faqs from '@/components/faqs/faqs'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getUniqueCategories } from '@/utils'
-import {getFavorites50Coupon} from "../../services"
+import { getFavorites50Coupon } from "../../services"
 
 
-const Top50CouponsAndDeals = async () => {
-  const favoritesCoupon  = await getFavorites50Coupon()
-  const categories = getUniqueCategories(favoritesCoupon)
-    
+const Top50CouponsAndDeals = () => {
+  const [favoritesCoupon, setFavoritesCoupon] = useState()
+  const [categories, setCategories] = useState()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true)
+      const favoritesCoupon = await getFavorites50Coupon()
+      const categories = getUniqueCategories(favoritesCoupon)
+      setFavoritesCoupon(favoritesCoupon)
+      setCategories(categories)
+      setLoading(false)
+    })()
+  }, [])
+
   return (
     <>
       <section className='container mx-auto px-4 lg:px-0 mt-5 md:mb-[50px]'>
@@ -26,12 +39,31 @@ const Top50CouponsAndDeals = async () => {
             <div className='flex-1'>
               <div className='flex flex-col gap-[25px] mb-20'>
                 {
-                  favoritesCoupon?.map((item, idx) => (
-                    <CouponList key={idx} logo item={item}/>
-                  ))
+                  loading ?
+                    [1, 2, 3]?.map((item, idx) => (
+                      <div role="status" key={idx} class="space-y-8 border rounded-xl overflow-hidden animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+                        <div class="flex items-center justify-center w-full h-36 bg-gray-300 rounded-sm sm:w-40 dark:bg-gray-500">
+                          <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                          </svg>
+                        </div>
+                        <div class="w-full p-5">
+                          <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-48 mb-4"></div>
+                          <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-500 max-w-[480px] mb-2.5"></div>
+                          <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-500 mb-2.5"></div>
+                          <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-500 max-w-[440px] mb-2.5"></div>
+                          <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-500 max-w-[460px] mb-2.5"></div>
+                        </div>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    ))
+                    :
+                    favoritesCoupon?.map((item, idx) => (
+                      <CouponList key={idx} logo item={item} />
+                    ))
                 }
               </div>
-              <Faqs name={favoritesCoupon?.[0]?.store?.Name}/>
+              <Faqs name={favoritesCoupon?.[0]?.store?.Name} />
             </div>
 
             <aside className='lg:w-[286px]'>
@@ -41,9 +73,23 @@ const Top50CouponsAndDeals = async () => {
                 className='flex flex-wrap gap-2.5'
               >
                 {
-                  categories?.map((item, idx) => (
-                    <CategoryButton key={idx} data={item} small />
-                  ))
+                  loading ?
+                    <div role="status" class="space-y-2.5 animate-pulse max-w-lg">
+                      <div class="flex items-center gap-2 flex-wrap w-[70%]">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+                        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-10"></div>
+                        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+                        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-6"></div>
+                        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-16"></div>
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-12"></div>
+                        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-4"></div>
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-14"></div>
+                      </div>
+                    </div>
+                   :
+                    categories?.map((item, idx) => (
+                      <CategoryButton key={idx} data={item} small />
+                    ))
                 }
               </div>
               <div className='mt-[30px]'>
@@ -63,7 +109,7 @@ const Top50CouponsAndDeals = async () => {
         </div>
 
         <div className='mt-16 mb-10'>
-          <Breadcrumb path={breadcrumbPath}/>
+          <Breadcrumb path={breadcrumbPath} />
         </div>
 
       </section>
@@ -76,6 +122,6 @@ export default Top50CouponsAndDeals
 const breadcrumbPath = [
   {
     label: 'Top 50 coupons and deals',
-    href:'/top-50-coupons-and-deals'
+    href: '/top-50-coupons-and-deals'
   }
 ]
