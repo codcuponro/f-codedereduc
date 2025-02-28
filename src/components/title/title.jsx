6ffa75d2000c9042c1738f93f61929886f533030
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-28 17:48:49 +0500

    update

--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -7,12 +7,12 @@ const Title = (props) => {
     <div className='flex flex-col md:flex-row md:justify-between items-start md:items-center gap-4 md:gap-10'>
         <h2 className={`text-2xl md:text-[34px] md:leading-[40px] font-semibold text-dark ${hideHeading && 'hidden md:block'} ${white && 'text-pure'}`}>{title}</h2>
         {
-          buttonLabel && <Link 
+          buttonLabel && <a 
               href={buttonHref} 
               className={` border rounded-[6px] text-center font-bold w-full md:w-auto py-2.5 px-[28px] ${hideButton && 'hidden md:block'}
                 ${white ? 'hover:bg-white text-pure hover:text-primary' : 'hover:bg-primary hover:text-white text-primary border-primary'}
               `}
-          >{buttonLabel}</Link>
+          >{buttonLabel}</a>
         }
     </div>
   )
--- src/config/Axios.js
+++ src/config/Axios.js
@@ -1,20 +1,14 @@
 export const Request = async (endpoint, options = {}) => {
   const url = `${process.env.NEXT_PUBLIC_BACKEND_PUBLIC_BASE_URL}/api${endpoint}`;
-  // const controller = new AbortController();
-  // const timeout = setTimeout(() => controller.abort(), 30000);
-
   try {
     const response = await fetch(url, {
       method: 'GET',
-      next: { revalidate: 0 },
+      // next: { revalidate: 60 },
       headers: {
         'Content-Type': 'application/json',
       },
-      // signal: controller.signal,
       ...options
     });
-
-    // clearTimeout(timeout);
     return await response.json();
   } catch (error) {
     console.error('Fetch error:', error);
