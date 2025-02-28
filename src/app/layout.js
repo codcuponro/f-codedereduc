2dc5e1fd6394003bda8dc44c2b7d3708ecbd50e9
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-28 22:02:20 +0500

    ToastProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from 'next/font/google'
 import Footer from "@/components/footer";
-
+import ToastProvider from "../context/ToastProvider"
 // Slider imports 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
