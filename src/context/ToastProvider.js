f53d4c9d2929be046309c78f7cb72a529b2d8837
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-03-13 22:32:23 +0500

    update

--- src/context/ToastProvider.js
+++ src/context/ToastProvider.js
@@ -27,6 +27,36 @@ const ToastProvider = ({ children }) => {
         }
     }, [])
 
+
+    useEffect(() => {
+        // Load Google Translate script
+        const googleTranslateScript = document.createElement("script");
+        googleTranslateScript.src =
+            "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        document.body.appendChild(googleTranslateScript);
+
+        // Initialize Google Translate
+        window.googleTranslateElementInit = () => {
+            new window.google.translate.TranslateElement(
+                { pageLanguage: "en", includedLanguages: "ro", autoDisplay: false },
+                "google_translate_element"
+            );
+        };
+
+        // Function to select Romanian automatically
+        const setDefaultLanguage = () => {
+            const selectEl = document.querySelector(".goog-te-combo");
+            if (selectEl) {
+                selectEl.value = "ro"; // Set Romanian
+                selectEl.dispatchEvent(new Event("change")); // Trigger change event
+            }
+        };
+
+        setTimeout(setDefaultLanguage, 3000); // Delay to allow the dropdown to load
+    }, []);
+
+
+
     return (
         <>
             <ToastContext.Provider value={showToast}>
@@ -38,6 +68,7 @@ const ToastProvider = ({ children }) => {
                 </Snackbar>
             </ToastContext.Provider>
             <ModalComponent />
+            {/* <div id="google_translate_element"></div>; */}
         </>
     );
 };
