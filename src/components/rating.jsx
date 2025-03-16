b03774faca3e9db686b9f9d1676d79d2d7258c96
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-03-16 21:19:13 +0500

    update

--- src/components/rating.jsx
+++ src/components/rating.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { FaStar } from 'react-icons/fa';
-import { FaRegStar } from "react-icons/fa";
+"use client"
+import React, { useState } from 'react';
+import { FaStar, FaRegStar } from 'react-icons/fa';
 
 const Rating = ({ totalRating = 5 }) => {
-    const maxStars = 5; // Total number of stars
+    const maxStars = 5;
+    const [hoveredStars, setHoveredStars] = useState(null);
+    const handleRating = (number) => {
+        alert(number)
+    }
+    
     return (
-        <div className='bg-[#FFA645] flex py-[5px] px-[10px] gap-[2px] rounded-full w-fit mt-[26px]'>
+        <div 
+            className="bg-[#FFA645] flex py-[5px] px-[10px] gap-[2px] rounded-full w-fit mt-[26px]"
+            onMouseLeave={() => setHoveredStars(null)} // Reset on leave
+        >
             {
                 [...Array(maxStars)].map((_, idx) => (
-                    idx < totalRating ? 
-                        <FaStar key={idx} className='w-[17px] text-white' /> 
-                        : <FaRegStar key={idx} className='w-[17px] text-white' />
+                    <span
+                        key={idx}
+                        onClick={()=>handleRating(idx+1)}
+                        onMouseEnter={() => setHoveredStars(idx + 1)}
+                    >
+                        {idx < (hoveredStars || totalRating) ? 
+                            <FaStar className="w-[17px] text-white cursor-pointer" /> 
+                            : <FaRegStar className="w-[17px] text-white cursor-pointer" />
+                        }
+                    </span>
                 ))
             }
         </div>
