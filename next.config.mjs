81a2ac281ca8a52e128cfbe31f8a3c1e8fab0fd9
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-04-25 23:27:07 +0500

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
   images: {
     domains: ["codcupon.nyc3.digitaloceanspaces.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'codcupon.ro' }],
+        destination: 'https://www.codcupon.ro/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
  
 export default nextConfig;
